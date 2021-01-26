8c22d4bbfa13ac2561eb12985e84fca6e2068f10
Charles <charlescampet03@gmail.com>
2021-01-26 23:03:55 +0100

    last commit

--- app_3.rb
+++ app_3.rb
@@ -4,6 +4,7 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+# ------------- Attention the program is in franglais ------#
     puts "------------------------------------------------"
     puts "|   Bienvenue sur 'ILS VEULENT TOUS MA POO' !   |"
     puts "|        Devenez l'Ace du ciel !                |"
@@ -13,7 +14,17 @@ require_relative 'lib/player'
     print "> "
     user_name = gets.chomp
     print "\n"
+    
     my_game = Game.new(user_name)
+    my_game.show_players
 
 
+    while my_game.is_still_ongoing?
+        my_game.menu
+        my_game.menu_choice(gets.chomp)
+        my_game.show_players
+        my_game.enemies_attack
+    end
+    my_game.end 
+    
 binding.pry
--- lib/game.rb
+++ lib/game.rb
@@ -27,17 +27,13 @@ class Game
     end
     
     def is_still_ongoing?
-        if @human_player.life > 0 && @enemies.empty? == false #Empty method return a boolean if array is empty (vide)
-            return true
-        else
-            false
-        end
+        @human_player.life > 0 && !@enemies.empty? ? true : false #Empty method return a boolean if array is empty (vide)
     end
 
     def show_players
         @human_player.show_state
         print "\n"
-        puts "#{@enemies.count} enemies rest !"
+        puts "#{@enemies.count} enemies rest ! \n"
     end
 
     def menu
@@ -48,20 +44,17 @@ class Game
         puts "s - chercher à se soigner"
         puts "------------------------------------"
         puts "attaquer un joueur en vue :"
+        i = 0
+        while i < @enemies.length
+            print "#{i} = #{@enemies[i].show_state}"
+            i += 1
+        end
         puts "------------------------------------"
-        print "0 - "
-        puts @enemies[0].show_state
-        print "1 - "
-        puts @enemies[1].show_state
-        print "2 - "
-        puts @enemies[2].show_state
-        print "3 - "
-        print @enemies[3].show_state
-        puts "------------------------------------"
+        print "\n> "
     end
 
     def menu_choice(string)
-        input = gets.chomp
+        input = string
         case input
         when "a"
             @human_player.search_weapon
@@ -78,19 +71,21 @@ class Game
                 kill_player(@enemies[1])
             end
         when "2"
-            @human_player.attaks(@enemies[2])
+            @human_player.attacks(@enemies[2])
             if @enemies[2].life <= 0
                 kill_player(@enemies[2])
             end
         when "3"
-            @human_player.attaks(@enemies[3])
+            @human_player.attacks(@enemies[3])
             if @enemies[3].life <= 0
                 kill_player(@enemies[3])
             end
         else
             puts "### APPRENDS A LIRE ZOZO ###"
         end
+        print "\n"
     end
+
     def enemies_attack
         if is_still_ongoing? == true
             @enemies.each do |pnj| 
@@ -98,14 +93,14 @@ class Game
             end
         end
     end
+
     def end
         puts "La partie est fini !"
+        
         if @human_player.life > 0
             puts "Bravo, tu as gagné"
-            puts @enemies.show_state @human_player.show_state
         else
             puts "Looser ! Tu as perdu !"
-            puts @enemies.show_state @human_player.show_state
-    end
+        end
     end
 end
